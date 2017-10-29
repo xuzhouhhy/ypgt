1e589040b07b3a917551b23a5ed028b38227c251
xiaosong_fu <xiaosong_fu@huacenav.com>
2017-10-29 10:22:22 +0800

    1：修复采集fragment基类

--- source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/sr/fragment/WellBaseFragment.java
+++ source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/sr/fragment/WellBaseFragment.java
@@ -1,30 +1,22 @@
 package com.geocraft.electrics.sr.fragment;
 
-import android.widget.LinearLayout;
-
 import com.geocraft.electrics.R;
 import com.geocraft.electrics.base.BusinessFragment;
-import com.geocraft.electrics.entity.DataSet;
 import com.geocraft.electrics.sr.WellActivity;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 
 /**
+ * 井号采集fragment基类
  */
 @EFragment(R.layout.fragment_business)
 public class WellBaseFragment extends BusinessFragment {
 
-    protected DataSet mDataSet;
-    protected Boolean mIsNew;
-    protected LinearLayout mLinearLayout;
-
-    @AfterViews
+    @Override
     protected void init() {
         mIsNew = ((WellActivity) this.getActivity()).getController().isCreateRecord();
         mDataSet = ((WellActivity) this.getActivity()).getController().getCurrentDataSet();
         super.initData(mIsNew, mDataSet);
-
     }
 }
 
