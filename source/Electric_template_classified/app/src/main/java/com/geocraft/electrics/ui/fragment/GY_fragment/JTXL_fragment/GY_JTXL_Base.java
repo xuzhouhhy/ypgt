4c6ec3e7dddfe491ce04c27a31605dc54784b875
xiaosong_fu <xiaosong_fu@huacenav.com>
2017-10-30 09:45:54 +0800

    1：更新模版文件

--- source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/fragment/GY_fragment/JTXL_fragment/GY_JTXL_Base.java
+++ source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/fragment/GY_fragment/JTXL_fragment/GY_JTXL_Base.java
@@ -15,27 +15,6 @@ public class GY_JTXL_Base extends WellBaseFragment {
     @ViewById
     LinearLayout linearLayoutRoot;
 
-//    @ViewById
-//    BusinessEditText etMC;
-//    @ViewById
-//    BusinessEditText etSJDW;
-//    @ViewById
-//    BusinessManager viewGLDW;
-
-    @Override
-    public boolean logicCheck() {
-//        //判断该充电桩已存在
-//        if(((RecordActivity)getActivity()).getController().isCreateRecord()) {
-//            DataSet dataSet = ((RecordActivity) getActivity()).getController().getCurrentDataSet();
-//            List<DataSet> dataSetList = dbManager.queryByCondition(dataSet, Enum.DATA_SET_TOWER_ID, etMC.getControlValue(), false);
-//            if (dataSetList.size() > 0) {
-//                T.show(getContext(), getString(R.string.exist_tower_id), Toast.LENGTH_SHORT);
-//                return false;
-//            }
-//        }
-
-        return true;
-    }
 
     @Override
     protected void init() {
