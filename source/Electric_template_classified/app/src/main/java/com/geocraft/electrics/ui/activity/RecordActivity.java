1158d3577f468c6b9b0ffeaa21e64e2ef8eded0c
xiaosong_fu <xiaosong_fu@huacenav.com>
2017-10-30 17:18:02 +0800

    处理相关异常

--- source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/activity/RecordActivity.java
+++ source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/activity/RecordActivity.java
@@ -2,6 +2,7 @@ package com.geocraft.electrics.ui.activity;
 
 import android.content.Intent;
 import android.support.v4.view.ViewPager;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -15,7 +16,9 @@ import com.geocraft.electrics.sr.SrPhotoManagerFragment;
 import com.geocraft.electrics.task.InitRecordInfoAsyncTask;
 import com.geocraft.electrics.task.RecordCommitAsyncTask;
 import com.geocraft.electrics.ui.adapter.RecordFragmentAdapter;
+import com.geocraft.electrics.ui.controller.PhotoManagerController;
 import com.geocraft.electrics.ui.controller.RecordController;
+import com.geocraft.electrics.ui.fragment.PhotoManagerFragment;
 import com.huace.log.logger.L;
 
 import org.androidannotations.annotations.AfterViews;
@@ -35,300 +38,311 @@ import java.util.List;
 @EActivity(R.layout.activity_record)
 @OptionsMenu(R.menu.menu_new_task)
 public class RecordActivity extends BaseActivity implements
-        RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener {
+		RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener {
 
-    public BusinessFragment mBasicDataFragment;
-    public BusinessFragment mNecessaryDataFragment;
-    public BusinessFragment mCoordinateFragment;
-    @Bean
-    RecordController mController;
-    @ViewById
-    RadioGroup radioGroupTab;
-    @ViewById
-    RadioButton radioButtonNecessary;
-    @ViewById
-    RadioButton radioButtonBasic;
-    @ViewById
-    RadioButton radioButtonPhoto;
-    @ViewById
-    RadioButton radioButtonCoordinate;
-    @ViewById
-    ViewPager viewPagerRecord;
-    SrPhotoManagerFragment mPhotoFragment;
-    RecordFragmentAdapter mAdapter;
+	@Bean
+	RecordController mController;
 
-    @AfterViews
-    void init() {
+	@ViewById
+	RadioGroup radioGroupTab;
 
-        mController.initIntentData(this);
+	@ViewById
+	RadioButton radioButtonNecessary;
 
-        InitRecordInfoAsyncTask initRecordInfoAsyncTask = new InitRecordInfoAsyncTask(this,
-                mController);
-        initRecordInfoAsyncTask.execute(mController);
-    }
+	@ViewById
+	RadioButton radioButtonBasic;
+
+	@ViewById
+	RadioButton radioButtonPhoto;
+
+	@ViewById
+	RadioButton radioButtonCoordinate;
 
 
-    public void initView() {
-        radioButtonNecessary.setChecked(true);
-        radioGroupTab.setOnCheckedChangeListener(this);
-        mAdapter = new RecordFragmentAdapter(getSupportFragmentManager());
-        mAdapter.setFragment(mNecessaryDataFragment, mBasicDataFragment, mPhotoFragment, mCoordinateFragment);
-        viewPagerRecord.setOffscreenPageLimit(mAdapter.getOffscreenPageLimit());
-        viewPagerRecord.setAdapter(mAdapter);
-        viewPagerRecord.addOnPageChangeListener(this);
+	@ViewById
+	ViewPager viewPagerRecord;
 
-        mController.decideRadioButtonPhotoVisibility(this);
-        mController.decideRadioButtonCoordinateVisibility(this);
-        mController.decideRadioButtonNecessaryVisibility(this);
-        mController.decideRadioButtonBasicVisibility(this);
-        this.setTitle(mController.getTitle());
-    }
+	public BusinessFragment mBasicDataFragment;
+	public BusinessFragment mNecessaryDataFragment;
+	public BusinessFragment mCoordinateFragment;
+	SrPhotoManagerFragment mPhotoFragment;
+	RecordFragmentAdapter mAdapter;
 
-    @OptionsItem
-    void actionTaskCommit() {
-        getValueFromFragment();
-        boolean isContinueCheck = mController.checkDataValidity(this, getPhotoInfoList());
-        if (isContinueCheck) {
-            if (null == mNecessaryDataFragment || !mNecessaryDataFragment.logicCheck()) {
-                return;
-            }
-            RecordCommitAsyncTask recordCommitAsyncTask = new RecordCommitAsyncTask(this,
-                    mController);
-            recordCommitAsyncTask.execute(mController);
-        }
-    }
+	@AfterViews
+	void init() {
 
-    public List<SrPhotoManagerController.PhotoItemInfo> getPhotoInfoList() {
-        List<SrPhotoManagerController.PhotoItemInfo> photoItemInfoList = new ArrayList<>();
-        if (mPhotoFragment != null) {
-            photoItemInfoList = mPhotoFragment.getTaskPhotoList();
-        }
-        return photoItemInfoList;
-    }
+		mController.initIntentData(this);
 
-    private void getValueFromFragment() {
-        if (mController.getCurrentDataSet() != null) {
-            if (mNecessaryDataFragment != null) {
-                mNecessaryDataFragment.getValue(mController.getCurrentDataSet());
-            }
-            if (mBasicDataFragment != null) {
-                mBasicDataFragment.getValue(mController.getCurrentDataSet());
-            }
-            if (mCoordinateFragment != null) {
-                mCoordinateFragment.getValue(mController.getCurrentDataSet());
-            }
-        }
-    }
-
-    public void initFragment() {
-        RecordController.DataFragmentGroup dataFragmentGroup = mController.getDataFragmentGroup();
-        if (dataFragmentGroup != null) {
-            mNecessaryDataFragment = dataFragmentGroup.mNecessaryDataFragment;
-            mBasicDataFragment = dataFragmentGroup.mBasicDataFragment;
-            mPhotoFragment = dataFragmentGroup.mPhotoManagerFragment;
-            mCoordinateFragment = dataFragmentGroup.mCoordinateFragment;
-        }
-    }
-
-    public RecordController getController() {
-        return mController;
-    }
-
-    private void changeFragment(int i) {
-        int j = 0;
-        viewPagerRecord.getAdapter().getCount();
-        switch (i) {
-            default:
-            case R.id.radioButtonNecessary: {
-                j = 0;
-            }
-            break;
-            case R.id.radioButtonBasic: {
-                if (mController.isShowNecessary()) {
-                    j = 1;
-                } else {
-                    j = 0;
-                }
-
-            }
-            break;
-            case R.id.radioButtonPhoto: {
-                if (mController.isShowNecessary()) {
-                    if (mController.isShowBasic()) {
-                        j = 2;
-                    } else {
-                        j = 1;
-                    }
-                } else {
-                    if (mController.isShowBasic()) {
-                        j = 1;
-                    } else {
-                        j = 0;
-                    }
-                }
-
-            }
-            break;
-            case R.id.radioButtonCoordinate: {
-                if (mController.isShowNecessary()) {
-                    if (mController.isShowBasic()) {
-                        if (mController.isShowPhoto()) {
-                            j = 3;
-                        } else {
-                            j = 2;
-                        }
-                    } else {
-                        if (mController.isShowPhoto()) {
-                            j = 2;
-                        } else {
-                            j = 1;
-                        }
-                    }
-                } else {
-                    if (mController.isShowBasic()) {
-                        if (mController.isShowPhoto()) {
-                            j = 2;
-                        } else {
-                            j = 1;
-                        }
-                    } else {
-                        if (mController.isShowPhoto()) {
-                            j = 1;
-                        } else {
-                            j = 0;
-                        }
-                    }
-                }
-            }
-            break;
-        }
-
-        viewPagerRecord.setCurrentItem(j);
-        mAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        changeFragment(checkedId);
-    }
+		InitRecordInfoAsyncTask initRecordInfoAsyncTask = new InitRecordInfoAsyncTask(this,
+				mController);
+		initRecordInfoAsyncTask.execute(mController);
+	}
 
 
-    public void setRadioButtonPhotoVisibility(int visibility) {
-        radioButtonPhoto.setVisibility(visibility);
-    }
 
-    public void setRadioButtonCoordinateVisibility(int visibility) {
-        radioButtonCoordinate.setVisibility(visibility);
-    }
+	public void initView() {
+		radioButtonNecessary.setChecked(true);
+		radioGroupTab.setOnCheckedChangeListener(this);
+		mAdapter = new RecordFragmentAdapter(getSupportFragmentManager());
+		mAdapter.setFragment(mNecessaryDataFragment, mBasicDataFragment, mPhotoFragment, mCoordinateFragment);
+		viewPagerRecord.setOffscreenPageLimit(mAdapter.getOffscreenPageLimit());
+		viewPagerRecord.setAdapter(mAdapter);
+		viewPagerRecord.addOnPageChangeListener(this);
 
-    public void setRadioButtonBasicVisibility(int visibility) {
-        radioButtonBasic.setVisibility(visibility);
-    }
+		mController.decideRadioButtonPhotoVisibility(this);
+		mController.decideRadioButtonCoordinateVisibility(this);
+		mController.decideRadioButtonNecessaryVisibility(this);
+		mController.decideRadioButtonBasicVisibility(this);
+		this.setTitle(mController.getTitle());
+	}
 
-    public void setRadioButtonNecessaryVisibility(int visibility) {
-        radioButtonNecessary.setVisibility(visibility);
-    }
+	@OptionsItem
+	void actionTaskCommit() {
+		getValueFromFragment();
+		boolean isContinueCheck = mController.checkDataValidity(this, getPhotoInfoList());
+		if (isContinueCheck) {
+			// TODO: 2017/10/24 原对象方法一直返回true,并且mNecessaryDataFragment为null没考虑，故注释掉
+//			if (mNecessaryDataFragment.logicCheck()) {
+//
+//			}
+			RecordCommitAsyncTask recordCommitAsyncTask = new RecordCommitAsyncTask(this,
+					mController);
+			recordCommitAsyncTask.execute(mController);
+		}
+	}
 
-    @Override
-    protected void onDestroy() {
-        //mController.clearPhotoCache();
-        super.onDestroy();
-    }
+	public List<SrPhotoManagerController.PhotoItemInfo> getPhotoInfoList() {
+		List<SrPhotoManagerController.PhotoItemInfo> photoItemInfoList = new ArrayList<>();
+		if (mPhotoFragment != null) {
+			photoItemInfoList = mPhotoFragment.getTaskPhotoList();
+		}
+		return photoItemInfoList;
+	}
 
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-    }
+	private void getValueFromFragment() {
+		if (mController.getCurrentDataSet() != null) {
+			if (mNecessaryDataFragment != null) {
+				mNecessaryDataFragment.getValue(mController.getCurrentDataSet());
+			}
+			if (mBasicDataFragment != null) {
+				mBasicDataFragment.getValue(mController.getCurrentDataSet());
+			}
+			if (mCoordinateFragment != null) {
+				mCoordinateFragment.getValue(mController.getCurrentDataSet());
+			}
+		}
+	}
 
-    @Override
-    public void onPageSelected(int position) {
+	public void initFragment() {
+		RecordController.DataFragmentGroup dataFragmentGroup = mController.getDataFragmentGroup();
+		if (dataFragmentGroup != null) {
+			mNecessaryDataFragment = dataFragmentGroup.mNecessaryDataFragment;
+			mBasicDataFragment = dataFragmentGroup.mBasicDataFragment;
+			mPhotoFragment = dataFragmentGroup.mPhotoManagerFragment;
+			mCoordinateFragment = dataFragmentGroup.mCoordinateFragment;
+		}
+	}
 
-        int i = viewPagerRecord.getCurrentItem();
+	public RecordController getController() {
+		return mController;
+	}
 
-        switch (i) {
-            default:
-            case 0: {
-                if (mController.isShowNecessary()) {
-                    radioButtonNecessary.setChecked(true);
-                    break;
-                } else if (mController.isShowBasic()) {
-                    radioButtonBasic.setChecked(true);
-                    break;
-                } else if (mController.isShowPhoto()) {
-                    radioButtonPhoto.setChecked(true);
-                    break;
-                } else if (mController.isShowCoordinate()) {
-                    radioButtonCoordinate.setChecked(true);
-                    break;
-                }
-            }
-            break;
-            case 1: {
-                if (mController.isShowBasic()) {
-                    radioButtonBasic.setChecked(true);
-                    break;
-                } else if (mController.isShowPhoto()) {
-                    radioButtonPhoto.setChecked(true);
-                    break;
-                } else if (mController.isShowCoordinate()) {
-                    radioButtonCoordinate.setChecked(true);
-                    break;
-                }
-            }
-            break;
-            case 2: {
-                if (mController.isShowPhoto()) {
-                    radioButtonPhoto.setChecked(true);
-                    break;
-                } else if (mController.isShowCoordinate()) {
-                    radioButtonCoordinate.setChecked(true);
-                    break;
-                }
-            }
-            break;
-            case 3: {
-                if (mController.isShowCoordinate()) {
-                    radioButtonCoordinate.setChecked(true);
-                    break;
-                }
-            }
-            break;
-        }
-    }
+	private void changeFragment(int i) {
+		int j = 0;
+		viewPagerRecord.getAdapter().getCount();
+		switch (i) {
+			default:
+			case R.id.radioButtonNecessary: {
+				j = 0;
+			}
+			break;
+			case R.id.radioButtonBasic: {
+				if (mController.isShowNecessary()) {
+					j = 1;
+				} else {
+					j = 0;
+				}
 
-    @Override
-    public void onPageScrollStateChanged(int state) {
-    }
+			}
+			break;
+			case R.id.radioButtonPhoto: {
+				if (mController.isShowNecessary()) {
+					if (mController.isShowBasic()) {
+						j = 2;
+					} else {
+						j = 1;
+					}
+				} else {
+					if (mController.isShowBasic()) {
+						j = 1;
+					} else {
+						j = 0;
+					}
+				}
 
-    public void initData(DataSet dataSet) {
-        mBasicDataFragment.initData(false, dataSet);
-        mNecessaryDataFragment.initData(false, dataSet);
-        mCoordinateFragment.initData(false, dataSet);
-    }
+			}
+			break;
+			case R.id.radioButtonCoordinate: {
+				if (mController.isShowNecessary()) {
+					if (mController.isShowBasic()) {
+						if (mController.isShowPhoto()) {
+							j = 3;
+						} else {
+							j = 2;
+						}
+					} else {
+						if (mController.isShowPhoto()) {
+							j = 2;
+						} else {
+							j = 1;
+						}
+					}
+				} else {
+					if (mController.isShowBasic()) {
+						if (mController.isShowPhoto()) {
+							j = 2;
+						} else {
+							j = 1;
+						}
+					} else {
+						if (mController.isShowPhoto()) {
+							j = 1;
+						} else {
+							j = 0;
+						}
+					}
+				}
+			}
+			break;
+		}
 
-    public void refreshViewPagerRecord() {
-        try {
-            if (mAdapter != null) {
-                mAdapter.notifyDataSetChanged();
-            }
-        } catch (Exception e) {
-            L.printException(e);
-        }
-    }
+		viewPagerRecord.setCurrentItem(j);
+		mAdapter.notifyDataSetChanged();
+	}
 
-    public BusinessFragment getNecessaryDataFragment() {
-        return this.mNecessaryDataFragment;
-    }
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		changeFragment(checkedId);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case ConstRequestCode.REQUEST_CODE_FINISH_RECORD_ACTIVITY: {
-                this.finish();
-                break;
-            }
-            default:
-                break;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+
+
+	public void setRadioButtonPhotoVisibility(int visibility) {
+		radioButtonPhoto.setVisibility(visibility);
+	}
+
+	public void setRadioButtonCoordinateVisibility(int visibility) {
+		radioButtonCoordinate.setVisibility(visibility);
+	}
+
+	public void setRadioButtonBasicVisibility(int visibility) {
+		radioButtonBasic.setVisibility(visibility);
+	}
+
+	public void setRadioButtonNecessaryVisibility(int visibility) {
+		radioButtonNecessary.setVisibility(visibility);
+	}
+
+	@Override
+	protected void onDestroy() {
+		//mController.clearPhotoCache();
+		super.onDestroy();
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+
+		int i = viewPagerRecord.getCurrentItem();
+
+		switch (i) {
+			default:
+			case 0: {
+				if (mController.isShowNecessary()) {
+					radioButtonNecessary.setChecked(true);
+					break;
+				} else if (mController.isShowBasic()) {
+					radioButtonBasic.setChecked(true);
+					break;
+				} else if (mController.isShowPhoto()) {
+					radioButtonPhoto.setChecked(true);
+					break;
+				} else if (mController.isShowCoordinate()) {
+					radioButtonCoordinate.setChecked(true);
+					break;
+				}
+			}
+			break;
+			case 1: {
+				if (mController.isShowBasic()) {
+					radioButtonBasic.setChecked(true);
+					break;
+				} else if (mController.isShowPhoto()) {
+					radioButtonPhoto.setChecked(true);
+					break;
+				} else if (mController.isShowCoordinate()) {
+					radioButtonCoordinate.setChecked(true);
+					break;
+				}
+			}
+			break;
+			case 2: {
+				if (mController.isShowPhoto()) {
+					radioButtonPhoto.setChecked(true);
+					break;
+				} else if (mController.isShowCoordinate()) {
+					radioButtonCoordinate.setChecked(true);
+					break;
+				}
+			}
+			break;
+			case 3: {
+				if (mController.isShowCoordinate()) {
+					radioButtonCoordinate.setChecked(true);
+					break;
+				}
+			}
+			break;
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+	}
+
+	public void initData(DataSet dataSet) {
+		mBasicDataFragment.initData(false, dataSet);
+		mNecessaryDataFragment.initData(false, dataSet);
+		mCoordinateFragment.initData(false, dataSet);
+	}
+
+	public void refreshViewPagerRecord() {
+		try {
+			if (mAdapter != null) {
+				mAdapter.notifyDataSetChanged();
+			}
+		} catch (Exception e) {
+			L.printException(e);
+		}
+	}
+
+	public BusinessFragment getNecessaryDataFragment() {
+		return this.mNecessaryDataFragment;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+			case ConstRequestCode.REQUEST_CODE_FINISH_RECORD_ACTIVITY: {
+				this.finish();
+				break;
+			}
+			default:
+				break;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 }
