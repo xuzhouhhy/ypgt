5113ac659c8a17af2537da104b922d50a912c280
xiaosong_fu <xiaosong_fu@huacenav.com>
2017-10-31 21:20:23 +0800

    1：增加关闭所

--- source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/fragment/GY_fragment/KBS_fragment/GY_KBS_GLKG.java
+++ source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/fragment/GY_fragment/KBS_fragment/GY_KBS_GLKG.java
@@ -3,8 +3,6 @@ package com.geocraft.electrics.ui.fragment.GY_fragment.KBS_fragment;
 import android.widget.LinearLayout;
 
 import com.geocraft.electrics.R;
-import com.geocraft.electrics.base.BusinessFragment;
-import com.geocraft.electrics.entity.DataSet;
 import com.geocraft.electrics.sr.fragment.WellBaseFragment;
 
 import org.androidannotations.annotations.EFragment;
@@ -16,27 +14,6 @@ public class GY_KBS_GLKG extends WellBaseFragment {
     @ViewById
     LinearLayout linearLayoutRoot;
 
-//    @ViewById
-//    BusinessEditText etMC;
-//    @ViewById
-//    BusinessEditText etSJDW;
-//    @ViewById
-//    BusinessManager viewGLDW;
-
-    @Override
-    public boolean logicCheck() {
-//        //判断该充电桩已存在
-//        if(((RecordActivity)getActivity()).getController().isCreateRecord()) {
-//            DataSet dataSet = ((RecordActivity) getActivity()).getController().getCurrentDataSet();
-//            List<DataSet> dataSetList = dbManager.queryByCondition(dataSet, Enum.DATA_SET_TOWER_ID, etMC.getControlValue(), false);
-//            if (dataSetList.size() > 0) {
-//                T.show(getContext(), getString(R.string.exist_tower_id), Toast.LENGTH_SHORT);
-//                return false;
-//            }
-//        }
-
-        return true;
-    }
 
     @Override
     protected void init() {
@@ -44,13 +21,4 @@ public class GY_KBS_GLKG extends WellBaseFragment {
         super.init();
     }
 
-
-    @Override
-    public void getValue(DataSet dataSet) {
-
-//        if(!viewGLDW.strSecondManager.isEmpty()) {
-//            etSJDW.setControlValue(viewGLDW.strSecondManager);
-//        }
-        super.getValue(dataSet);
-    }
 }
