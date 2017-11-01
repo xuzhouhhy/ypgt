e9ad66fd99814f7806a57605ca5ff04c7a313c2f
xiaosong_fu <xiaosong_fu@huacenav.com>
2017-11-01 10:26:02 +0800

    1:架空线路增加一杆六照界面

---
+++ source/Electric_template_classified/app/src/main/java/com/geocraft/electrics/ui/fragment/GY_fragment/JTXL_fragment/GY_JTXL_YGLZ.java
@@ -0,0 +1,24 @@
+package com.geocraft.electrics.ui.fragment.GY_fragment.JTXL_fragment;
+
+import android.widget.LinearLayout;
+
+import com.geocraft.electrics.R;
+import com.geocraft.electrics.sr.fragment.WellBaseInfoFragment;
+
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+@EFragment(R.layout.fragment_gy__jtxl__yglz)
+public class GY_JTXL_YGLZ extends WellBaseInfoFragment {
+
+    @ViewById
+    LinearLayout linearLayoutRoot;
+
+
+    @Override
+    protected void init() {
+        mLinearLayout = linearLayoutRoot;
+        super.init();
+    }
+
+}
